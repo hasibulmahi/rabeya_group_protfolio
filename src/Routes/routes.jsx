70a95e5a284c2b47c01bbfd491e184566577fa6b
Hasibul Mahi <hasibulmahi@gmail.com>
2024-04-04 03:05:00 +0600

    Update routes.jsx

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,58 +1,54 @@
-import {
-    createBrowserRouter
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import LayoutMain from "../Layouts/MainLayOut/LayoutMain.jsx";
 import Services from "../Pages/Services/Services.jsx";
 import Projects from "../Pages/Projects/Projects.jsx";
 import Teams from "../Pages/Teams/Teams.jsx";
-// import Home from "../Pages/Home/Home/home.jsx";
+import Home from "../Pages/Home/Home/Home.jsx";
 import ServicesPage from "../Pages/ServicesPage/servicesPage.jsx";
 import OurProjectPage from "../Pages/OurProjectPage/OurProjectPage.jsx";
 import OurTeamPage from "../Pages/OurTeamPage/OurTeamPage.jsx";
 import ContactUsPage from "../Pages/ContactUsPage/ContactUsPage.jsx";
-import Home from '../../Pages/Home/Home/home.jsx';
 
 const routes = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <LayoutMain></LayoutMain>,
+    children: [
+      {
         path: "/",
-        element: <LayoutMain></LayoutMain> ,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: 'servicesPage',
-                element: <ServicesPage></ServicesPage>
-            },
-            {
-                path:'ourTeamsPage',
-                element: <OurTeamPage></OurTeamPage>
-            },
-            {
-                path: 'ourProjectsPage',
-                element:<OurProjectPage></OurProjectPage>
-            },
-            {
-                path: 'contactUsPage',
-                element:<ContactUsPage></ContactUsPage>
-
-            },
-            {
-                path: 'services',
-                element: <Services></Services>
-            },
-            {
-                path: 'ourProject',
-                element:<Projects></Projects>
-            },
-            {
-                path: 'ourTeam',
-                element:<Teams></Teams>
-            }
-        ]
-    },
+        element: <Home></Home>,
+      },
+      {
+        path: "servicesPage",
+        element: <ServicesPage></ServicesPage>,
+      },
+      {
+        path: "ourTeamsPage",
+        element: <OurTeamPage></OurTeamPage>,
+      },
+      {
+        path: "ourProjectsPage",
+        element: <OurProjectPage></OurProjectPage>,
+      },
+      {
+        path: "contactUsPage",
+        element: <ContactUsPage></ContactUsPage>,
+      },
+      {
+        path: "services",
+        element: <Services></Services>,
+      },
+      {
+        path: "ourProject",
+        element: <Projects></Projects>,
+      },
+      {
+        path: "ourTeam",
+        element: <Teams></Teams>,
+      },
+    ],
+  },
 ]);
 
 export default routes;
